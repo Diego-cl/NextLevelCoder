0a58ab61ebadb5a3e061e7474a2faadc2067d4ca
Diego-cl <74841625+Diego-cl@users.noreply.github.com>
2020-11-26 19:03:30 -0400

    clase 2

---
+++ utils/constants.py
@@ -0,0 +1,11 @@
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 480
+TITLE = "Next Lever Coder"
+
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
